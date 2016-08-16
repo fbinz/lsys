052d5bae4b5af168f94e04a88cfd265b78d65618
Fabian <fabian.binz@gmail.com>
2016-08-17 00:31:48 +0200

    Added loadable examples

--- lsys.py
+++ lsys.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -17,8 +18,13 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(splitter)
 
         sub_layout = QFormLayout()
-        sub_layout.setWidget(0, QFormLayout.SpanningRole, QLabel('L-System Demo'))
         sub_layout.setLabelAlignment(Qt.AlignRight)
+        sub_layout.setWidget(0, QFormLayout.SpanningRole, QLabel('L-System Demo'))
+        self.load_examples_box = QComboBox()
+        for s in MainWindow.EXAMPLES.keys():
+            self.load_examples_box.addItem(s)
+        self.load_examples_box.currentTextChanged.connect(self.load_example)
+        sub_layout.addRow('Load Example:', self.load_examples_box)
         self.iteration_input = QSpinBox()
         self.iteration_input.setMaximum(20)
         sub_layout.addWidget(self.iteration_input)
@@ -44,23 +50,27 @@ class MainWindow(QMainWindow):
         self.rule_list.setSelectionMode(QListWidget.SingleSelection)
         group_layout.addWidget(self.rule_list)
         group.setLayout(group_layout)
-        sub_layout.setWidget(5, QFormLayout.SpanningRole, group)
+        sub_layout.setWidget(6, QFormLayout.SpanningRole, group)
         refresh_button = QPushButton('Refresh')
         refresh_button.clicked.connect(self.refresh_display)
         group = QGroupBox('Drawing')
         group_layout = QFormLayout()
         group_layout.setLabelAlignment(Qt.AlignRight)
-        self.draw_characters = QLineEdit('FG')
-        group_layout.addRow('Draw', self.draw_characters)
-        self.skip_characters = QLineEdit('fg')
-        group_layout.addRow('Skip', self.skip_characters)
-        self.turn_left_characters = QLineEdit('+')
-        group_layout.addRow('Turn Left', self.turn_left_characters)
-        self.turn_right_characters = QLineEdit('-')
-        group_layout.addRow('Turn Right', self.turn_right_characters)
+        self.starting_angle_input = QSpinBox()
+        self.starting_angle_input.setMaximum(180)
+        self.starting_angle_input.setMinimum(-180)
+        group_layout.addRow('Starting Angle [°]', self.starting_angle_input)
+        self.draw_characters_input = QLineEdit('FG')
+        group_layout.addRow('Draw', self.draw_characters_input)
+        self.skip_characters_input = QLineEdit('fg')
+        group_layout.addRow('Skip', self.skip_characters_input)
+        self.turn_left_characters_input = QLineEdit('+')
+        group_layout.addRow('Turn Left', self.turn_left_characters_input)
+        self.turn_right_characters_input = QLineEdit('-')
+        group_layout.addRow('Turn Right', self.turn_right_characters_input)
         group.setLayout(group_layout)
-        sub_layout.setWidget(6, QFormLayout.SpanningRole, group)
-        sub_layout.setWidget(7, QFormLayout.SpanningRole, refresh_button)
+        sub_layout.setWidget(7, QFormLayout.SpanningRole, group)
+        sub_layout.setWidget(8, QFormLayout.SpanningRole, refresh_button)
 
         dummy = QWidget()
         dummy.setLayout(sub_layout)
@@ -84,6 +94,8 @@ class MainWindow(QMainWindow):
 
         # create status bar
         self.statusBar()
+
+        self.load_examples_box.setCurrentText('Fractal plant')
 
     def add_rule(self):
         item = QListWidgetItem()
@@ -141,13 +153,13 @@ class MainWindow(QMainWindow):
         # draw turtle graphics
         stack = []
         angle_increment = math.radians(int(self.angle_increment.text()))
-        angle = 0
+        angle = self.starting_angle_input.value()
         p0 = np.array((0, 0))
         pos = []
-        draw_chars = self.draw_characters.text()
-        skip_chars = self.skip_characters.text()
-        left_chars = self.turn_left_characters.text()
-        right_chars = self.turn_right_characters.text()
+        draw_chars = self.draw_characters_input.text()
+        skip_chars = self.skip_characters_input.text()
+        left_chars = self.turn_left_characters_input.text()
+        right_chars = self.turn_right_characters_input.text()
         for c in drawing:
             if c in draw_chars:
                 p1 = p0 + np.array((math.sin(angle), math.cos(angle)))
@@ -172,6 +184,163 @@ class MainWindow(QMainWindow):
 
         self.statusBar().showMessage('Finished drawing {} vertices.'.format(len(pos)), 5000)
         self.line.set_data(pos=pos)
+
+    def clear_all(self):
+        self.rule_list.clear()
+        self.angle_increment.setValue(0)
+        self.draw_characters_input.clear()
+        self.axiom_input.clear()
+        self.skip_characters_input.clear()
+        self.turn_left_characters_input.clear()
+        self.turn_right_characters_input.clear()
+
+    def load_example(self, name):
+        example = MainWindow.EXAMPLES.get(name)
+        if not example:
+            return
+
+        self.clear_all()
+        for rule in example['rules']:
+            self.rule_list.addItem(QListWidgetItem(rule))
+        self.axiom_input.setText(example.get('axiom'))
+        self.draw_characters_input.setText(example.get('draw'))
+        self.skip_characters_input.setText(example.get('skip'))
+        self.turn_right_characters_input.setText(example.get('turn_right'))
+        self.turn_left_characters_input.setText(example.get('turn_left'))
+        self.angle_increment.setValue(example.get('angle_increment') or 0)
+        self.starting_angle_input.setValue(example.get('starting_angle') or 0)
+        self.iteration_input.setValue(example.get('N') or 0)
+
+    EXAMPLES = {
+        'Pytagoras Tree': {
+            'rules': [
+                '1 -> 11',
+                '0 -> 1[+0]-0'
+            ],
+            'axiom': '0',
+            'draw': '01',
+            'angle_increment': 45,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 5,
+        },
+        'Koch curve': {
+            'rules': [
+                'F -> F+F-F-F+F'
+            ],
+            'axiom': 'F',
+            'draw': 'F',
+            'angle_increment': 90,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 5,
+        },
+        'Sierpinski triangle': {
+            'rules': [
+                'A -> +B-A-B+',
+                'B -> -A+B+A-'
+            ],
+            'axiom': 'A',
+            'draw': 'AB',
+            'angle_increment': 60,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 5,
+        },
+        'Dragon curve': {
+            'rules': [
+                'X -> X+YF+',
+                'Y -> -FX-Y'
+            ],
+            'axiom': 'FX',
+            'draw': 'F',
+            'angle_increment': 90,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 5,
+        },
+        'Fractal plant': {
+            'rules': [
+                'X -> F-[[X]+X]+F[+FX]-X',
+                'F -> FF'
+            ],
+            'axiom': 'X',
+            'draw': 'F',
+            'angle_increment': 25,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': -25,
+            'N': 6,
+        },
+        'Fractal bushy thing': {
+            'rules': [
+                'F -> FF-[-F+F+F]+[+F-F-F]',
+            ],
+            'axiom': 'F',
+            'draw': 'F',
+            'angle_increment': 22.5,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 4,
+        },
+        'Another plant...': {
+            'rules': [
+                'F -> F[+F]F[-F][F]',
+            ],
+            'axiom': 'F',
+            'draw': 'F',
+            'angle_increment': 20,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 5,
+        },
+        'FASS example': {
+            'rules': [
+                'L->LF+RFR+FL-F-LFLFL-FRFR+',
+                'R->-LFLF+RFRFR+F+RF-LFL-FR',
+            ],
+            'axiom': '-L',
+            'draw': 'F',
+            'angle_increment': 90,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 3,
+        },
+        'Island and lakes': {
+            'rules': [
+                'F -> F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF',
+                'f -> fffffff'
+            ],
+            'axiom': 'F+F+F+F',
+            'draw': 'F',
+            'skip': 'f',
+            'angle_increment': 90,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 2,
+        },
+        'Round Koch curve': {
+            'rules': [
+                'F -> FF-F-F-F-F-F+F',
+            ],
+            'axiom': 'F-F-F-F',
+            'draw': 'F',
+            'angle_increment': 90,
+            'turn_left': '+',
+            'turn_right': '-',
+            'starting_angle': 0,
+            'N': 4,
+        },
+    }
+
 
 
 if __name__ == '__main__':
